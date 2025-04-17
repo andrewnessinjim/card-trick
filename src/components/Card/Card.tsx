ec69b6304ffa40f276202be224c78d5f82da3a25
Andrew Nessin <andrewnessinjim@yahoo.com>
2025-04-17 11:40:05 +0530

    Initial Commit

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { motion } from "motion/react";
+
+import CardBack from "@/cards/CardBack";
+import Clubs2 from "@/cards/Clubs2";
+import * as React from "react";
+import styled, { css } from "styled-components";
+
+const cardIdMap = {
+  "2C": Clubs2,
+};
+
+type Status = "faceUp" | "faceDown";
+export type CardId = keyof typeof cardIdMap;
+
+interface Props {
+  id: CardId;
+  initStatus?: Status;
+}
+
+function Card({ id, initStatus = "faceDown" }: Props) {
+  const [status, setStatus] = React.useState<Status>(initStatus);
+  const CardElement = cardIdMap[id];
+
+  console.log({ status });
+  return (
+    <Wrapper
+      initial={{ rotateY: status === "faceUp" ? 0 : 180 }}
+      animate={{ rotateY: status === "faceUp" ? 0 : 180 }}
+      transition={{ type: "spring", duration: 1, bounce: 0.4 }}
+      onClick={() => {
+        console.log("click");
+        setStatus((prev) => (prev === "faceUp" ? "faceDown" : "faceUp"));
+      }}
+    >
+      <FrontFace>
+        <CardElement height={250} />
+      </FrontFace>
+      <BackFace>
+        <CardBack height={250} />
+      </BackFace>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled(motion.div)`
+  width: fit-content;
+  position: relative;
+  transform-style: preserve-3d;
+`;
+
+const Face = css`
+  backface-visibility: hidden;
+  width: 100%;
+  height: 100%;
+`;
+
+const FrontFace = styled.div`
+  ${Face}
+`;
+
+const BackFace = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  ${Face}
+  transform: rotateY(180deg);
+`;
+
+export default Card;
---
+++ src/components/Game/Game.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+
+import * as React from "react";
+import Card from "../Card";
+
+function Game() {
+  return <Card id="2C" />;
+}
+
+export default Game;
